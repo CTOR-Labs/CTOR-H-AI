2200ee795b208dfec1e0d79674c9b4c82f899432
CTOR‑Federation. <bva5@hotmail.com>
2026-04-03 19:23:12 +0200

    Update postcss.config.js

--- mvp/lovable/postcss.config.js
+++ mvp/lovable/postcss.config.js
@@ -1,1 +1,8 @@
-gitdir: /nix/store/d8kp178jwyjy9qpcp1mil96k3binp3yc-template-pool/git/repo.git/worktrees/dev-server
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+
